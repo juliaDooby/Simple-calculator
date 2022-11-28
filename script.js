db69c5ff389059c3d21d16765c10c18aee54fb0c
Julia <87147748+Miroshnichenko22@users.noreply.github.com>
2022-11-29 00:16:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var vue = new Vue({
+    el: '#app',
+    data: {
+        result: '',
+        numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+        operations: ['+', '-', '*', '/'],
+    },
+    methods: {
+        input: function(char) {
+            this.result += char;
+            this.result = this.result.toString();
+        },
+        reset: function() {
+            this.result = '';
+        },
+        calc: function() {
+            this.result = eval(this.result);
+        },
+    },
+});
+
+// result: '', - строка, eval(this.result) - делает математической
